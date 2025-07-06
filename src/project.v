ce0b721244374e9147a4d1ea1f59f92da2237924
yoshiki9636 <61720120+yoshiki9636@users.noreply.github.com>
2025-07-06 09:31:34 +0900

    change top name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module ttihp25b-tt_um_yoshiki9636_my-riscv-rv32i (
+module tt_um_yoshiki9636_my-riscv-rv32i (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
